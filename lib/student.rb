2961e5910d08320143bec2d10a8e9deb931ca5b8
danny galimi <dannygalimi13@aim.com>
2021-02-23 03:44:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,10 +11,10 @@ end
 
 def self.create_table
   sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS songs (
+      CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
-        album TEXT
+        grade INTEGER
         )
         SQL
     DB[:conn].execute(sql) 
