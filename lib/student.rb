2e7d7adc24f8e7993c40cf3cf04d8c50d44ca912
danny galimi <dannygalimi13@aim.com>
2021-02-23 03:48:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,9 @@ end
   
   def save
     sql =  <<-SQL
-      
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
   end 
 end
